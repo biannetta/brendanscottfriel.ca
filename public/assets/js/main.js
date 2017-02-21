66c49c8db667164ca7d5644f6a3d0fd8760b76c4
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2017-02-20 20:33:00 -0500

    Updating website for Released Material

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,6 @@
 (function() {
    var artist = document.getElementsByClassName('artist-name')[0];
    var title  = document.getElementsByClassName('album-title')[0];
-   var city   = document.getElementsByClassName('city')[0];
-   var fb     = document.getElementsByClassName('fb-link')[0];
-   var sc     = document.getElementsByClassName('sc-link')[0];
 
    var reveal = function(elem, easeIn) {
      var cls = '';
@@ -17,7 +14,4 @@
 
    setTimeout(function(){reveal(artist)}, 500);
    setTimeout(function(){reveal(title)}, 1200);
-   setTimeout(function(){reveal(city)}, 1200);
-   setTimeout(function(){reveal(fb, 'shift')}, 1200);
-   setTimeout(function(){reveal(sc, 'shift')}, 1200);
 })();
