2fd1447ef7734fb7e3d78f59a0cacfcbfef737ed
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2018-02-21 22:32:07 -0500

    Moving File Structure
    Moved all the assets into top level src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var minifyCSS = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('generate-css', function() {
-
+gulp.task('css', function() {
+    return gulp.src('src/css/*.css')
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('build/css'));
 });
+gulp.task('images', function() {
+    return gulp.src('src/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/assets/images'));
+})
+
+gulp.task('default',['css','images']);
