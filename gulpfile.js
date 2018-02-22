0eef1a7ee7828dcc6ff7892ac925be661c2df34b
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2018-02-21 22:56:15 -0500

    Removing unused JS file - updated gitignore to ignore build folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,15 @@ var imagemin = require('gulp-imagemin');
 gulp.task('css', function() {
     return gulp.src('src/css/*.css')
         .pipe(minifyCSS())
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('build/assets/css'));
 });
 gulp.task('images', function() {
-    return gulp.src('src/assets/images/*')
+    return gulp.src('src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('build/assets/images'));
-})
-
-gulp.task('default',['css','images']);
+});
+gulp.task('js', function(){
+    return gulp.src('src/js/*.js')
+        .pipe(gulp.dest('build/assets/js'));
+});
+gulp.task('default',['css','images','js']);
