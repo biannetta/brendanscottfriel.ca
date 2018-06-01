630b3284183d01db51b0053b9715282430941a76
biannetta <benjamin.iannetta@gmail.com>
2018-06-01 17:02:35 -0400

    Overhauling design of website Simpler flow and layout

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var token = '3016048789.28578ca.c7bf8046303941fda6ea61b6f97e5eee',
   userid = 3016048789,
-  num_photos = 30; 
+  num_photos = 4; 
 
   $.ajax({
     url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
@@ -11,23 +11,13 @@ $(document).ready(function() {
     success: function(data){
       var list = $('#instafeed');
       for( x in data.data ){
-        list.append('<div data-aos="flip-up"><a href="'+data.data[x].images.thumbnail.url+'" target="_blank"><img src="'+data.data[x].images.low_resolution.url+'"></a></div>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+        var image = data.data[x];
+        console.log(image);
+        list.append('<a style="text-decoration: none;" href="'+image.link+'" target="_blank"><div data-likes="'+image.likes.count+'" class="instagram pic" style="background-image:url('+image.images.low_resolution.url+');"><span class="overlay"><i class="fas fa-heart"></i> '+image.likes.count+'</span></div></a>');
       }
     },
     error: function(data){
       console.log(data); // send the error notifications to console
     }
-  });
-
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 19) {
-      $('.music-hero').fadeOut();
-    } else {
-      $('.music-hero').fadeIn();
-    }
-  });
-
-  AOS.init({
-    duration: 1200,
   });
 });
